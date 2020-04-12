25f0e7170bbb49b30ec3fc216306ccb534726bcb
Olexyy <olexyy.mails@gmail.com>
2020-04-13 00:24:22 +0300

    Finalizing build

--- frontend/src/components/Main.vue
+++ frontend/src/components/Main.vue
@@ -1,6 +1,6 @@
 <template>
     <main class="mdl-layout__content">
-    <div class="page-content"><!-- Your content goes here -->
+    <div class="page-content">
       <div class="mdl-grid">
         <div class="demo-card-wide mdl-card mdl-shadow--2dp mdl-cell mdl-cell--12-col">
           <div class="mdl-card__title">
@@ -14,39 +14,15 @@
             Write topic and click start to indicate discussion starts.
           </div>
           <div class="mdl-card__actions mdl-card--border"></div>
-          <topic/>  
-          <cards/>
-          <users/>
+          <Topic/>  
+          <Cards/>
+          <Users/>
           <div class="mdl-card__actions mdl-card--border">
             <a id="change_name" v-on:click="onChangeNameClick" class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
               Change name
             </a>
           </div>
-          <div class="mdl-card__menu">
-            <span v-if="time" id="timer">{{time}}</span>
-            <button
-              v-if="discuss === 'result' || (discuss === 'discuss' && !anyUnvoted)"
-              id="discuss" 
-              v-on:click="onDiscussClick"
-              class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
-              Clear data
-            </button>
-            <button
-              v-else-if="discuss === 'discuss'"
-              id="discuss" 
-              v-on:click="onDiscussClick"
-              class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
-              Stop discussion
-            </button>
-            <button
-              :disabled="!topic && discuss === 'idle'"
-              v-else
-              id="discuss" 
-              v-on:click="onDiscussClick"
-              class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
-              Start discussion
-            </button>
-          </div>
+          <Menu/>
         </div>
       </div>
     </div>
@@ -57,45 +33,24 @@
   import Users from './Users.vue'
   import Cards from './Cards.vue'
   import Topic from './Topic.vue'
+  import Menu from './Menu.vue'
   export default {
     name: 'Main',
     components: {
       Users,
       Cards,
-      Topic
+      Topic,
+      Menu
     },
     computed: {
       room() {
         return this.$store.state.room;
-      },
-      time() {
-        return this.$store.state.time;
-      },
-      discuss() {
-        return this.$store.state.discuss;
-      },
-      anyUnvoted() {
-         return this.$store.state.anyUnvoted;
-      },
-      topic() {
-        return this.$store.state.topic;
       }
     },
     methods: {
       onChangeNameClick(e) {
         this.$store.state.dialogs.name.showModal();
       },
-      onDiscussClick(e) {
-				if (this.$store.state.discuss === 'idle') {
-					this.$store.state.socket.emit('discuss', this.$store.state.room, 'discuss');
-				}
-        else if (this.$store.state.discuss === 'discuss') { 
-					this.$store.state.socket.emit('discuss', this.$store.state.room, 'result');
-				}
-				else {
-					this.$store.state.socket.emit('clear', this.$store.state.room);
-				}
-      }
     }
   }
 </script>
---
+++ frontend/src/components/Menu.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="mdl-card__menu">
+    <span v-if="time" id="timer">{{time}}</span>
+    <button
+      v-if="discuss === 'result' || (discuss === 'discuss' && !anyUnvoted)"
+      id="discuss" 
+      v-on:click="onDiscussClick"
+      class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+      Clear data
+    </button>
+    <button
+      v-else-if="discuss === 'discuss'"
+      id="discuss" 
+      v-on:click="onDiscussClick"
+      class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+      Stop discussion
+    </button>
+    <button
+      :disabled="!topic && discuss === 'idle'"
+      v-else
+      id="discuss" 
+      v-on:click="onDiscussClick"
+      class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+      Start discussion
+    </button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Menu',
+    computed: {
+      time() {
+        return this.$store.state.time;
+      },
+      discuss() {
+        return this.$store.state.discuss;
+      },
+      anyUnvoted() {
+         return this.$store.state.anyUnvoted;
+      },
+      topic() {
+        return this.$store.state.topic;
+      }
+    },
+    methods: {
+      onDiscussClick(e) {
+				if (this.$store.state.discuss === 'idle') {
+					this.$store.state.socket.emit('discuss', this.$store.state.room, 'discuss');
+				}
+        else if (this.$store.state.discuss === 'discuss') { 
+					this.$store.state.socket.emit('discuss', this.$store.state.room, 'result');
+				}
+				else {
+					this.$store.state.socket.emit('clear', this.$store.state.room);
+				}
+      }
+    }
+  }
+</script>
