8ac657c3a73768cbb8e9d6b23807240f5025ba43
Olexyy <olexyy.mails@gmail.com>
2020-05-12 22:45:21 +0300

    backend app class

--- server/app.js
+++ server/app.js
@@ -1,116 +1,79 @@
-const express    = require('express');
-const path       = require('path');
-const port       = process.env.PORT || 3000;
-const app        = express();
-const fs         = require('fs');
-var http         = require('http').createServer(app);
-const storage    = path.join(__dirname, "..", 'store');
+class App {
 
-/**
- * WebSocket Configuration
- */
-var io = require('socket.io')(http, {});
+    constructor(io) {
+        this.io = io;
+        this.players = io.of('/pocker');
+        this.app = {
+            rooms: {},
+            socketMap: {}
+        };
+    }
 
-/**
-* Pocker backend implementation.
-*/
-const pockerPlayers = io.of('/pocker');
-const pockerApp = {
-    rooms: {},
-    socketMap: {}
-};
-pockerPlayers.on('connect', function(socket) {
-    console.log('pocker connected');
-    socket.on('disconnect', function() {
-        if (pockerApp.socketMap.hasOwnProperty(socket.id)){
-            const room = pockerApp.socketMap[socket.id];
-            delete(pockerApp[room].players[socket.id]);
-            delete(pockerApp.socketMap[socket.id]);
-            pockerPlayers.to(room).emit('status', pockerApp[room]);
-            console.log('pocker player disconnected, room' + room);
-        }
-    });
-    socket.on('error', e => {
-        if (pockerApp.socketMap.hasOwnProperty(socket.id)) {
-            const room = pockerApp.socketMap[socket.id];
-            delete(pockerApp[room].players[socket.id]);
-            delete(pockerApp.socketMap[socket.id]);
-            console.log('pocker socket error, room' + room);
-        }
-    });
-    socket.on('update', (room, prop, value) => {
-        if (!pockerApp.hasOwnProperty(room)) {
-            pockerApp[room] = {
+    bind() {
+        this.players.on('connect', socket => {
+            socket.on('update', (room, prop, value) => {
+                this.ensureRoom(room);
+                this.ensureUser(room, socket);
+                this.app[room].players[socket.id][prop] = value;
+                this.players.to(room).emit('status', this.app[room]);
+            });
+            socket.on('clear', room => {
+                this.app[room].discuss = 'idle';
+                this.app[room].topic = '';
+                Object.values(this.app[room].players).forEach(player => {
+                    player.vote = '';
+                });
+                this.players.to(room).emit('status', this.app[room]);
+            });
+            socket.on('topic', (room, value) => {
+                this.app[room].topic = value;
+                this.players.to(room).emit('status', this.app[room]);
+            });
+            socket.on('discuss', (room, value) => {
+                this.app[room].discuss = value;
+                this.players.to(room).emit('status', this.app[room]);
+            });
+            socket.on('sound', room => {
+                this.players.to(room).emit('sound');
+            });
+            socket.on('liveness', e => {
+                console.log(`socket ${socket.id} is live`);
+            });
+            socket.on('disconnect', () => {
+                this.deleteUser(socket.id);
+            });
+            socket.on('error', e => {
+                this.deleteUser(socket.id);
+            });
+        });
+    }
+
+    ensureRoom(room) {
+        if (!this.app.hasOwnProperty(room)) {
+            this.app[room] = {
                 players: {},
                 topic: '',
                 discuss: 'idle'
             };
         }
-        if (!pockerApp[room].players.hasOwnProperty(socket.id)) {
-            pockerApp[room].players[socket.id] = {name:'', vote: ''};
-            pockerApp.socketMap[socket.id] = room;
+    }
+
+    ensureUser(room, socket) {
+        if (!this.app[room].players.hasOwnProperty(socket.id)) {
+            this.app[room].players[socket.id] = {name:'', vote: ''};
+            this.app.socketMap[socket.id] = room;
             socket.join(room);
         }
-        pockerApp[room].players[socket.id][prop] = value;
-        pockerPlayers.to(room).emit('status', pockerApp[room]);
-        console.log('pocker update updated, room' + room);
-    });
-    socket.on('clear', room => {
-        pockerApp[room].discuss = 'idle';
-        pockerApp[room].topic = '';
-        Object.values(pockerApp[room].players).forEach(player => {
-            player.vote = '';
-        });
-        pockerPlayers.to(room).emit('status', pockerApp[room]);
-        console.log('pocker clear updated, room' + room);
-    });
-    socket.on('topic', (room, value) => {
-        pockerApp[room].topic = value;
-        pockerPlayers.to(room).emit('status', pockerApp[room]);
-        console.log('pocker topic updated, room' + room);
-    });
-    socket.on('discuss', (room, value) => {
-        pockerApp[room].discuss = value;
-        pockerPlayers.to(room).emit('status', pockerApp[room]);
-        console.log('pocker discuss updated, room' + room);
-    });
-    socket.on('sound', (room) => {
-        pockerPlayers.to(room).emit('sound');
-        console.log('pocker sound, room' + room);
-    });
-    socket.on('liveness', e => {
-        console.log('socket is live');
-    });
-});
-/**
- *   Storage
- */
-app.use(express.static(path.join(storage)));
-
-app.get('/', function (req, res) {
-    res.sendFile(path.join(storage+'/index.html'));
-});
-
-app.get('/room/:id', function (req, res) {
-    res.sendFile(path.join(storage+'/index.html'));
-});
-
-app.get('/:file', function (req, res) {
-    const fileName = req.params.file;
-    const filePath = path.join(storage, `${fileName}.html`);
-    if (fs.existsSync(filePath)) {
-      res.sendFile(filePath);
     }
-    else {
-      res.status(404).end('Not found');
+
+    deleteUser(id) {
+        if (this.app.socketMap.hasOwnProperty(id)){
+            const room = this.app.socketMap[id];
+            delete(this.app[room].players[id]);
+            delete(this.app.socketMap[id]);
+            this.players.to(room).emit('status', this.app[room]);
+        }
     }
-});
+}
 
-http.listen(port, () => console.log(`\x1b[40m`,`\x1b[32m`,
-`
-    [+] Server         : http://0.0.0.0:${port}
-    [+] Storage Path   : ${storage}
-
-    [~] Running Server...
-
-`,`\x1b[0m`));
+module.exports = App;
