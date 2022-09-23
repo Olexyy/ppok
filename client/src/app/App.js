4a673f4eb4ad8bc25dd22e6a1fcfcc37ddd39e7e
Olexyy <olexyy.mails@gmail.com>
2022-09-23 12:36:35 +0300

    Change sound.

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -167,7 +167,7 @@ class App {
     }
 
     playSound() {
-        let src = '/pop_50.mp3';
+        let src = '/sound2.mp3';
         let audio = new Audio(src);
         audio.play();
     }
@@ -178,7 +178,7 @@ class App {
     // eslint-disable-next-line no-unused-vars
     trigger(name, data, context) {
         if (name === 'sound') {
-            let src = '/pop_50.mp3';
+            let src = '/sound2.mp3';
             let audio = new Audio(src);
             audio.play();
         }
